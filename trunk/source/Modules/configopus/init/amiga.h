356d63cab4b35f581d8c7976f0ef15c5d881acae
kas1e <kas1e@fbfc52eb-d3ae-4490-b8fb-8982a1084939>
2013-09-27 07:56:11 +0000

    configopus.module: fixed dummy kprintf define

--- trunk/source/Modules/configopus/init/amiga.h
+++ trunk/source/Modules/configopus/init/amiga.h
@@ -128,7 +128,7 @@ int stccpy(char *p, const char *q, int n);
   #define KPrintF(fmt, args...)  {DebugPrintF("[%s:%ld %s] ", __FILE__, __LINE__, __FUNCTION__); DebugPrintF(fmt, ##args);}  // just in case anyone will use KPrintF and do not want to add -ldebug.
  #endif
 #else
- #define KPrintF(fmt, args...) VOID(0)
+ #define KPrintF(fmt, args...) ((void)0)
 #endif
 
 #endif
