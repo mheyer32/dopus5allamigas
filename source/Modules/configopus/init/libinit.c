f04c3377065cc8a19623468d44391a17f9101391
paraxenic <paraxenic@fbfc52eb-d3ae-4490-b8fb-8982a1084939>
2013-09-27 04:17:30 +0000

    amiga.c - moved GlobalStuffChar declaration from lsprintf.c to amiga.c           moved LSprintf() function from lsprintf.c to amiga.c libinit.c - Added copyright string (defined in version/dopus_version.h) libinit.c - Changed version strings and numbers to use definitions in             version/dopus_version.h. module_deps.h - Changed version string definitions to use definitions in                 version/dopus_version.h makefiles - added date defines for use in program version strings. removed unused files lsprinf.c & version.h

--- source/Modules/configopus/init/libinit.c
+++ source/Modules/configopus/init/libinit.c
@@ -21,7 +21,7 @@
 ***************************************************************************/
 #include <stdlib.h>
 
-#include "init/amiga.h"
+#include "amiga.h"
 
 #include <proto/exec.h>
 #include <proto/dos.h>
@@ -66,6 +66,7 @@ int _start(void) //must be first for 68k library
 // stack cookie for shell v45+
 static const char USED_VAR stack_size[] = "$STACK:" STR(MIN_STACKSIZE) "\n";
 #endif
+static const char USED_VAR copyright[] = COPYRIGHT;
 
 /****************************************************************************/
 
