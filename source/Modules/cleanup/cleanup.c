1ac0f1cc50a5afb12fe9b2b33445c33d6623b310
paraxenic <paraxenic@fbfc52eb-d3ae-4490-b8fb-8982a1084939>
2013-11-02 18:31:20 +0000

    Moved includes that are also used by modules from Program to Include/dopus. Updated Program/dopus.h with new location of moved includes. Added mod_base.h to Include/dopus and removed some modules base.h. Added new common makefiles to some modules. Updated include statements in changed moduels.

--- source/Modules/cleanup/cleanup.c
+++ source/Modules/cleanup/cleanup.c
@@ -22,9 +22,9 @@ For more information on Directory Opus for Windows please see:
 */
 
 #include "cleanup.h"
-#include <Program/position.h>
-#include <Program/main_commands.h>
 
+#include <dopus/position.h>
+#include <dopus/main_commands.h>
 
 
 char *version="$VER: cleanup.module 68.0 (3.10.98)";
