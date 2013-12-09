146ecda35b4781716af64ef8fb7e9d07ffe73409
bszili <bszili@fbfc52eb-d3ae-4490-b8fb-8982a1084939>
2013-12-09 18:31:47 +0000

    Show iconified windows in Workbench too.

--- source/Library/wb.h
+++ source/Library/wb.h
@@ -26,199 +26,244 @@ For more information on Directory Opus for Windows please see:
 #if defined(__amigaos4__)
 
 #define LIBCALL_0(ret,func,callbase,callface) \
-	( (ret ASM (*) (APTR __local_iface))func) \
+	((ret ASM (*) (APTR __local_iface))func) \
 	(callface)
-
 
 #define LIBCALL_1(ret,func,callbase,callface,r1,n1) \
 	((ret ASM (*) (APTR __local_iface, REG(r1, ULONG)))func) \
 	(callface, (ULONG)n1)
 
-
-#define LIBCALL_2(ret,func,callbase,callface,r1,n1, r2,n2) \
+#define LIBCALL_2(ret,func,callbase,callface,r1,n1,r2,n2) \
 	((ret ASM (*) (APTR __local_iface, REG(r1, ULONG), REG(r2, ULONG)))func) \
 	(callface, (ULONG)n1, (ULONG)n2)
-	
-#define LIBCALL_3(ret,func,callbase,callface,r1,n1, r2,n2, r3,n3) \
+
+#define LIBCALL_3(ret,func,callbase,callface,r1,n1,r2,n2,r3,n3) \
 	((ret ASM (*) (APTR __local_iface, REG(r1, ULONG), REG(r2, ULONG), REG(r3, ULONG)))func) \
 	(callface, (ULONG)n1, (ULONG)n2, (ULONG)n3)
-	
 
-#define LIBCALL_5(ret,func,callbase,callface,r1,n1, r2,n2, r3,n3, r4,n4, r5,n5) \
-	((ret ASM (*) (APTR __local_iface, REG(r1, ULONG), REG(r2, ULONG), REG(r3, ULONG), REG(r4, ULONG), REG(r5,ULONG)))func) \
+#define LIBCALL_5(ret,func,callbase,callface,r1,n1,r2,n2,r3,n3,r4,n4,r5,n5) \
+	((ret ASM (*) (APTR __local_iface, REG(r1, ULONG), REG(r2, ULONG), REG(r3, ULONG), REG(r4, ULONG), REG(r5, ULONG)))func) \
 	(callface, (ULONG)n1, (ULONG)n2, (ULONG)n3, (ULONG)n4, (ULONG)n5)
-		
-	
+
+#define LIBCALL_7(ret,func,callbase,callface,r1,n1,r2,n2,r3,n3,r4,n4,r5,n5,r6,n6,r7,n7) \
+	((ret ASM (*) (APTR __local_iface, REG(r1, ULONG), REG(r2, ULONG), REG(r3, ULONG), REG(r4, ULONG), REG(r5, ULONG), REG(r6, ULONG), REG(r7, ULONG)))func) \
+	(callface, (ULONG)n1, (ULONG)n2, (ULONG)n3, (ULONG)n4, (ULONG)n5, (ULONG)n6, (ULONG)n7)
+
 #define PATCH(jumptable_dummy,offset,func,type) { offset,  &func##_stubs , type }
 
-
-#define PATCHED_0(ret,name) ret name##_stubs (APTR __local_iface) { \
-	return (ret)name(); } ret name(void) {
-
+#define PATCHED_0(ret,name) \
+	ret name##_stubs (APTR __local_iface) \
+	{ \
+		return (ret)name(); \
+	} \
+	ret name(void) {
 	
 #define PATCHED_1(ret,name,r1,t1,n1) \
-   ret name##_stubs(APTR __local_iface, REG(r1, t1 n1)) \
-  { \
-     extern ret name(REG(r1, t1 n1)); \
-     return (ret)name(n1); \
-  } \
-  ret name(t1 n1) {
-	
+	ret name##_stubs(APTR __local_iface, REG(r1, t1 n1)) \
+	{ \
+		extern ret name(REG(r1, t1 n1)); \
+		return (ret)name(n1); \
+	} \
+	ret name(t1 n1) {
 
 #define PATCHED_2(ret,name,r1,t1,n1,r2,t2,n2) \
-   ret name##_stubs(APTR __local_iface, REG(r1, t1 n1),REG(r2, t2 n2)) \
-  { \
-     extern ret name(REG(r1, t1 n1),REG(r2, t2 n2)); \
-     return (ret)name(n1, n2); \
-  } \
-  ret name(t1 n1, t2 n2) {
+	ret name##_stubs(APTR __local_iface, REG(r1, t1 n1),REG(r2, t2 n2)) \
+	{ \
+		extern ret name(REG(r1, t1 n1),REG(r2, t2 n2)); \
+		return (ret)name(n1, n2); \
+	} \
+	ret name(t1 n1, t2 n2) {
 
+#define PATCHED_3(ret,name,r1,t1,n1,r2,t2,n2,r3,t3,n3) \
+	ret name##_stubs(APTR __local_iface, REG(r1, t1 n1),REG(r2, t2 n2),REG(r3,t3 n3)) \
+	{ \
+		extern ret name(REG(r1, t1 n1),REG(r2, t2 n2), REG(r3, t3 n3)); \
+		return (ret)name(n1, n2 , n3); \
+	} \
+	ret name(t1 n1, t2 n2, t3 n3) {
 
-#define PATCHED_3(ret,name,r1,t1,n1,r2,t2,n2, r3,t3,n3) \
-   ret name##_stubs(APTR __local_iface, REG(r1, t1 n1),REG(r2, t2 n2),REG(r3,t3 n3)) \
-  { \
-     extern ret name(REG(r1, t1 n1),REG(r2, t2 n2), REG(r3, t3 n3)); \
-     return (ret)name(n1, n2 , n3); \
-  } \
-  ret name(t1 n1, t2 n2, t3 n3) {
+#define PATCHED_5(ret,name,r1,t1,n1,r2,t2,n2 r3,t3,n3,r4,t4,n4,r5,t5,n5) \
+	ret name##_stubs(APTR __local_iface, REG(r1, t1 n1),REG(r2, t2 n2),REG(r3,t3 n3), REG(r4, t4 n4), REG(r5, t5 n5)) \
+	{ \
+		extern ret name(REG(r1, t1 n1),REG(r2, t2 n2), REG(r3, t3 n3), REG(r4, t4 n4), REG(r5, t5 n5)); \
+		return (ret)name(n1, n2 , n3, n4, n5); \
+	} \
+	ret name(t1 n1, t2 n2, t3 n3, t4 n4, t5 n5) {
 
-
-#define PATCHED_5(ret,name,r1,t1,n1,r2,t2,n2, r3,t3,n3, r4,t4,n4, r5,t5,n5) \
-   ret name##_stubs(APTR __local_iface, REG(r1, t1 n1),REG(r2, t2 n2),REG(r3,t3 n3), REG(r4, t4 n4), REG(r5, t5 n5)) \
-  { \
-     extern ret name(REG(r1, t1 n1),REG(r2, t2 n2), REG(r3, t3 n3), REG(r4, t4 n4), REG(r5, t5 n5)); \
-     return (ret)name(n1, n2 , n3, n4, n5); \
-  } \
-  ret name(t1 n1, t2 n2, t3 n3, t4 n4, t5 n5) {
-
-
-#define PATCHED_6(ret,name,r1,t1,n1,r2,t2,n2, r3,t3,n3, r4,t4,n4, r5,t5,n5, r6,t6,n6) \
-   ret name##_stubs(APTR __local_iface, REG(r1, t1 n1),REG(r2, t2 n2),REG(r3,t3 n3), REG(r4, t4 n4), REG(r5, t5 n5), REG(r6, t6 n6)) \
-  { \
-     extern ret name(REG(r1, t1 n1),REG(r2, t2 n2), REG(r3, t3 n3), REG(r4, t4 n4), REG(r5, t5 n5), REG(r6, t6 n6)); \
-     return (ret)name(n1, n2 , n3, n4, n5, n6); \
-  } \
-  ret name(t1 n1, t2 n2, t3 n3, t4 n4, t5 n5, t6 n6) {
-
+#define PATCHED_6(ret,name,r1,t1,n1,r2,t2,n2,r3,t3,n3,r4,t4,n4,r5,t5,n5,r6,t6,n6) \
+	ret name##_stubs(APTR __local_iface, REG(r1, t1 n1),REG(r2, t2 n2),REG(r3,t3 n3), REG(r4, t4 n4), REG(r5, t5 n5), REG(r6, t6 n6)) \
+	{ \
+		extern ret name(REG(r1, t1 n1),REG(r2, t2 n2), REG(r3, t3 n3), REG(r4, t4 n4), REG(r5, t5 n5), REG(r6, t6 n6)); \
+		return (ret)name(n1, n2 , n3, n4, n5, n6); \
+	} \
+	ret name(t1 n1, t2 n2, t3 n3, t4 n4, t5 n5, t6 n6) {
   
-#define PATCHED_7(ret,name,r1,t1,n1,r2,t2,n2, r3,t3,n3, r4,t4,n4, r5,t5,n5, r6,t6,n6, r7,t7,n7) \
-   ret name##_stubs(APTR __local_iface, REG(r1, t1 n1),REG(r2, t2 n2),REG(r3,t3 n3), REG(r4, t4 n4), REG(r5, t5 n5), REG(r6, t6 n6), REG(r7, t7 n7)) \
-  { \
-     extern ret name(REG(r1, t1 n1),REG(r2, t2 n2), REG(r3, t3 n3), REG(r4, t4 n4), REG(r5, t5 n5), REG(r6, t6 n6), REG(r7, t7 n7)); \
-     return (ret)name(n1, n2 , n3, n4, n5, n6, n7); \
-  } \
-  ret name(t1 n1, t2 n2, t3 n3, t4 n4, t5 n5, t6 n6, t7 n7) {
-
-
+#define PATCHED_7(ret,name,r1,t1,n1,r2,t2,n2,r3,t3,n3,r4,t4,n4,r5,t5,n5,r6,t6,n6,r7,t7,n7) \
+	ret name##_stubs(APTR __local_iface, REG(r1, t1 n1),REG(r2, t2 n2),REG(r3,t3 n3), REG(r4, t4 n4), REG(r5, t5 n5), REG(r6, t6 n6), REG(r7, t7 n7)) \
+	{ \
+		extern ret name(REG(r1, t1 n1),REG(r2, t2 n2), REG(r3, t3 n3), REG(r4, t4 n4), REG(r5, t5 n5), REG(r6, t6 n6), REG(r7, t7 n7)); \
+		return (ret)name(n1, n2 , n3, n4, n5, n6, n7); \
+	} \
+	ret name(t1 n1, t2 n2, t3 n3, t4 n4, t5 n5, t6 n6, t7 n7) {
 
 #elif defined(__MORPHOS__)
 
 #define LIBCALL_0(ret,func,callbase,dummy) \
-({ \
-	REG_A6 = (ULONG)callbase; \
-	(ret) (*MyEmulHandle->EmulCallDirect68k)(func); \
-})
-
+	({ \
+		REG_A6 = (ULONG)callbase; \
+		(ret) (*MyEmulHandle->EmulCallDirect68k)(func); \
+	})
 
 #define LIBCALL_1(ret,func,callbase,dummy,r1,n1) \
-({ \
-	REG_##r1 = (ULONG)n1; \
-	REG_A6 = (ULONG)callbase; \
-	(ret) (*MyEmulHandle->EmulCallDirect68k)(func); \
-})
-
-
+	({ \
+		REG_##r1 = (ULONG)n1; \
+		REG_A6 = (ULONG)callbase; \
+		(ret) (*MyEmulHandle->EmulCallDirect68k)(func); \
+	})
 
 #define LIBCALL_2(ret,func,callbase,dummy,r1,n1,r2,n2) \
-({ \
-	REG_##r1 = (ULONG)n1; \
-	REG_##r2 = (ULONG)n2; \
-	REG_A6 = (ULONG)callbase; \
-	(ret) (*MyEmulHandle->EmulCallDirect68k)(func); \
-})
+	({ \
+		REG_##r1 = (ULONG)n1; \
+		REG_##r2 = (ULONG)n2; \
+		REG_A6 = (ULONG)callbase; \
+		(ret) (*MyEmulHandle->EmulCallDirect68k)(func); \
+	})
+
 #define LIBCALL_3(ret,func,callbase,dummy,r1,n1,r2,n2,r3,n3) \
-({ \
-	REG_##r1 = (ULONG)n1; \
-	REG_##r2 = (ULONG)n2; \
-	REG_##r3 = (ULONG)n3; \
-	REG_A6 = (ULONG)callbase; \
-	(ret) (*MyEmulHandle->EmulCallDirect68k)(func); \
-})
+	({ \
+		REG_##r1 = (ULONG)n1; \
+		REG_##r2 = (ULONG)n2; \
+		REG_##r3 = (ULONG)n3; \
+		REG_A6 = (ULONG)callbase; \
+		(ret) (*MyEmulHandle->EmulCallDirect68k)(func); \
+	})
+
 #define LIBCALL_5(ret,func,callbase,dummy,r1,n1,r2,n2,r3,n3,r4,n4,r5,n5) \
-({ \
-	REG_##r1 = (ULONG)n1; \
-	REG_##r2 = (ULONG)n2; \
-	REG_##r3 = (ULONG)n3; \
-	REG_##r4 = (ULONG)n4; \
-	REG_##r5 = (ULONG)n5; \
-	REG_A6 = (ULONG)callbase; \
-	(ret) (*MyEmulHandle->EmulCallDirect68k)(func); \
-})
+	({ \
+		REG_##r1 = (ULONG)n1; \
+		REG_##r2 = (ULONG)n2; \
+		REG_##r3 = (ULONG)n3; \
+		REG_##r4 = (ULONG)n4; \
+		REG_##r5 = (ULONG)n5; \
+		REG_A6 = (ULONG)callbase; \
+		(ret) (*MyEmulHandle->EmulCallDirect68k)(func); \
+	})
+
+#define LIBCALL_7(ret,func,callbase,dummy,r1,n1,r2,n2,r3,n3,r4,n4,r5,n5,r6,n6,r7,n7) \
+	({ \
+		REG_##r1 = (ULONG)n1; \
+		REG_##r2 = (ULONG)n2; \
+		REG_##r3 = (ULONG)n3; \
+		REG_##r4 = (ULONG)n4; \
+		REG_##r5 = (ULONG)n5; \
+		REG_##r6 = (ULONG)n6; \
+		REG_##r7 = (ULONG)n7; \
+		REG_A6 = (ULONG)callbase; \
+		(ret) (*MyEmulHandle->EmulCallDirect68k)(func); \
+	})
 
 #define PATCH(offset,iface_dummy,func,type) { offset, { TRAP_LIB, 0, (APTR)&func }, type }
 
-#define PATCHED_0(ret,name) ret name (void) {
-#define PATCHED_1(ret,name,r1,t1,n1) ret name (void) { \
-	t1 n1 = (t1)REG_##r1;
-#define PATCHED_2(ret,name,r1,t1,n1,r2,t2,n2) ret name (void) { \
-	t1 n1 = (t1)REG_##r1; \
-	t2 n2 = (t2)REG_##r2;
-#define PATCHED_3(ret,name,r1,t1,n1,r2,t2,n2,r3,t3,n3) ret name (void) { \
-	t1 n1 = (t1)REG_##r1; \
-	t2 n2 = (t2)REG_##r2; \
-	t3 n3 = (t3)REG_##r3;
-#define PATCHED_5(ret,name,r1,t1,n1,r2,t2,n2,r3,t3,n3,r4,t4,n4,r5,t5,n5) ret name (void) { \
-	t1 n1 = (t1)REG_##r1; \
-	t2 n2 = (t2)REG_##r2; \
-	t3 n3 = (t3)REG_##r3; \
-	t4 n4 = (t4)REG_##r4; \
-	t5 n5 = (t5)REG_##r5;
-#define PATCHED_6(ret,name,r1,t1,n1,r2,t2,n2,r3,t3,n3,r4,t4,n4,r5,t5,n5,r6,t6,n6) ret name (void) { \
-	t1 n1 = (t1)REG_##r1; \
-	t2 n2 = (t2)REG_##r2; \
-	t3 n3 = (t3)REG_##r3; \
-	t4 n4 = (t4)REG_##r4; \
-	t5 n5 = (t5)REG_##r5; \
-	t6 n6 = (t6)REG_##r6;
-#define PATCHED_7(ret,name,r1,t1,n1,r2,t2,n2,r3,t3,n3,r4,t4,n4,r5,t5,n5,r6,t6,n6,r7,t7,n7) ret name (void) { \
-	t1 n1 = (t1)REG_##r1; \
-	t2 n2 = (t2)REG_##r2; \
-	t3 n3 = (t3)REG_##r3; \
-	t4 n4 = (t4)REG_##r4; \
-	t5 n5 = (t5)REG_##r5; \
-	t6 n6 = (t6)REG_##r6; \
-	t7 n7 = (t7)REG_##r7;
+#define PATCHED_0(ret,name) \
+	ret name(void) {
+
+#define PATCHED_1(ret,name,r1,t1,n1) \
+	ret name(void) { \
+		t1 n1 = (t1)REG_##r1;
+
+#define PATCHED_2(ret,name,r1,t1,n1,r2,t2,n2) \
+	ret name(void) { \
+		t1 n1 = (t1)REG_##r1; \
+		t2 n2 = (t2)REG_##r2;
+
+#define PATCHED_3(ret,name,r1,t1,n1,r2,t2,n2,r3,t3,n3) \
+	ret name(void) { \
+		t1 n1 = (t1)REG_##r1; \
+		t2 n2 = (t2)REG_##r2; \
+		t3 n3 = (t3)REG_##r3;
+
+#define PATCHED_5(ret,name,r1,t1,n1,r2,t2,n2,r3,t3,n3,r4,t4,n4,r5,t5,n5) \
+	ret name(void) { \
+		t1 n1 = (t1)REG_##r1; \
+		t2 n2 = (t2)REG_##r2; \
+		t3 n3 = (t3)REG_##r3; \
+		t4 n4 = (t4)REG_##r4; \
+		t5 n5 = (t5)REG_##r5;
+
+#define PATCHED_6(ret,name,r1,t1,n1,r2,t2,n2,r3,t3,n3,r4,t4,n4,r5,t5,n5,r6,t6,n6) \
+	ret name(void) { \
+		t1 n1 = (t1)REG_##r1; \
+		t2 n2 = (t2)REG_##r2; \
+		t3 n3 = (t3)REG_##r3; \
+		t4 n4 = (t4)REG_##r4; \
+		t5 n5 = (t5)REG_##r5; \
+		t6 n6 = (t6)REG_##r6;
+
+#define PATCHED_7(ret,name,r1,t1,n1,r2,t2,n2,r3,t3,n3,r4,t4,n4,r5,t5,n5,r6,t6,n6,r7,t7,n7) \
+	ret name(void) { \
+		t1 n1 = (t1)REG_##r1; \
+		t2 n2 = (t2)REG_##r2; \
+		t3 n3 = (t3)REG_##r3; \
+		t4 n4 = (t4)REG_##r4; \
+		t5 n5 = (t5)REG_##r5; \
+		t6 n6 = (t6)REG_##r6; \
+		t7 n7 = (t7)REG_##r7;
 
 #else
 
-#define LIBCALL_0(ret,func,callbase,dummy) ((ret ASM (*) (REG(a6, APTR)))func) ((APTR)callbase)
-#define LIBCALL_1(ret,func,callbase,dummy,r1,n1) ((ret ASM (*) (REG(r1, ULONG), REG(a6, APTR)))func) ((ULONG)n1, (APTR)callbase)
+#define LIBCALL_0(ret,func,callbase,dummy) \
+	((ret ASM (*) (REG(a6, APTR)))func) \
+	((APTR)callbase)
+
+#define LIBCALL_1(ret,func,callbase,dummy,r1,n1) \
+	((ret ASM (*) (REG(r1, ULONG), REG(a6, APTR)))func) \
+	((ULONG)n1, (APTR)callbase)
+
 #define LIBCALL_2(ret,func,callbase,dummy,r1,n1,r2,n2) \
 	((ret ASM (*) (REG(r1, ULONG), REG(r2, ULONG), REG(a6, APTR)))func) \
-	((ULONG)n1,(ULONG)n2,(APTR)callbase)
+	((ULONG)n1, (ULONG)n2, (APTR)callbase)
+
 #define LIBCALL_3(ret,func,callbase,dummy,r1,n1,r2,n2,r3,n3) \
 	((ret ASM (*) (REG(r1, ULONG), REG(r2, ULONG), REG(r3, ULONG), REG(a6, APTR)))func) \
-	((ULONG)n1,(ULONG)n2,(ULONG)n3,(APTR)callbase)
+	((ULONG)n1, (ULONG)n2, (ULONG)n3, (APTR)callbase)
+
 #define LIBCALL_5(ret,func,callbase,dummy,r1,n1,r2,n2,r3,n3,r4,n4,r5,n5) \
 	((ret ASM (*) (REG(r1, ULONG), REG(r2, ULONG), REG(r3, ULONG), REG(r4, ULONG), REG(r5, ULONG), REG(a6, APTR)))func) \
-	((ULONG)n1,(ULONG)n2,(ULONG)n3,(ULONG)n4,(ULONG)n5,(APTR)callbase)
+	((ULONG)n1, (ULONG)n2, (ULONG)n3, (ULONG)n4, (ULONG)n5, (APTR)callbase)
+
+#define LIBCALL_7(ret,func,callbase,dummy,r1,n1,r2,n2,r3,n3,r4,n4,r5,n5,r6,n6,r7,n7) \
+	((ret ASM (*) (REG(r1, ULONG), REG(r2, ULONG), REG(r3, ULONG), REG(r4, ULONG), REG(r5, ULONG), REG(r6, ULONG), REG(r7, ULONG), REG(a6, APTR)))func) \
+	((ULONG)n1, (ULONG)n2, (ULONG)n3, (ULONG)n4, (ULONG)n5, (ULONG)n6, (ULONG)n7, (APTR)callbase)
 
 #define PATCH(offset,iface_dummy,func,type) { offset, func, type }
 
-#define PATCHED_0(ret,name) ret name (void) {
-#define PATCHED_1(ret,name,r1,t1,n1) ret name ( REG(r1, t1 n1)) {
-#define PATCHED_2(ret,name,r1,t1,n1,r2,t2,n2) ret name ( REG(r1, t1 n1), REG(r2, t2 n2 )) {
-#define PATCHED_3(ret,name,r1,t1,n1,r2,t2,n2,r3,t3,n3) ret name ( REG(r1, t1 n1), REG(r2, t2 n2 ), REG(r3, t3 n3)) {
-#define PATCHED_5(ret,name,r1,t1,n1,r2,t2,n2,r3,t3,n3,r4,t4,n4,r5,t5,n5) ret name (REG(r1, t1 n1), REG(r2, t2 n2 ), REG(r3, t3 n3), REG(r4, t4 n4), REG(r5, t5 n5)) {
-#define PATCHED_6(ret,name,r1,t1,n1,r2,t2,n2,r3,t3,n3,r4,t4,n4,r5,t5,n5,r6,t6,n6) ret name (REG(r1, t1 n1), REG(r2, t2 n2 ), REG(r3, t3 n3), REG(r4, t4 n4), REG(r5, t5 n5), REG(r6, t6 n6)) {
-#define PATCHED_7(ret,name,r1,t1,n1,r2,t2,n2,r3,t3,n3,r4,t4,n4,r5,t5,n5,r6,t6,n6,r7,t7,n7) ret name (REG(r1, t1 n1), REG(r2, t2 n2 ), REG(r3, t3 n3), REG(r4, t4 n4), REG(r5, t5 n5), REG(r6, t6 n6), REG(r7, t7 n7)) {
+#define PATCHED_0(ret,name) \
+	ret name(void) {
+
+#define PATCHED_1(ret,name,r1,t1,n1) \
+	ret name(REG(r1, t1 n1)) {
+
+#define PATCHED_2(ret,name,r1,t1,n1,r2,t2,n2) \
+	ret name(REG(r1, t1 n1), REG(r2, t2 n2 )) {
+
+#define PATCHED_3(ret,name,r1,t1,n1,r2,t2,n2,r3,t3,n3) \
+	ret name(REG(r1, t1 n1), REG(r2, t2 n2 ), REG(r3, t3 n3)) {
+
+#define PATCHED_5(ret,name,r1,t1,n1,r2,t2,n2,r3,t3,n3,r4,t4,n4,r5,t5,n5) \
+	ret name(REG(r1, t1 n1), REG(r2, t2 n2 ), REG(r3, t3 n3), REG(r4, t4 n4), REG(r5, t5 n5)) {
+
+#define PATCHED_6(ret,name,r1,t1,n1,r2,t2,n2,r3,t3,n3,r4,t4,n4,r5,t5,n5,r6,t6,n6) \
+	ret name(REG(r1, t1 n1), REG(r2, t2 n2 ), REG(r3, t3 n3), REG(r4, t4 n4), REG(r5, t5 n5), REG(r6, t6 n6)) {
+
+#define PATCHED_7(ret,name,r1,t1,n1,r2,t2,n2,r3,t3,n3,r4,t4,n4,r5,t5,n5,r6,t6,n6,r7,t7,n7) \
+	ret name(REG(r1, t1 n1), REG(r2, t2 n2 ), REG(r3, t3 n3), REG(r4, t4 n4), REG(r5, t5 n5), REG(r6, t6 n6), REG(r7, t7 n7)) {
 
 #endif
 
 #define PATCH_END }
 
 
-#ifndef __AROS__
+#ifndef LIB_VECTSIZE
 // assume AmigaOS binary compatible
 #define LIB_VECTSIZE 6
 #endif
