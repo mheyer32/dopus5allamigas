852a8a7ae33ac9fd9b22828b093ac46f2c00909c
bszili <bszili@fbfc52eb-d3ae-4490-b8fb-8982a1084939>
2014-04-13 13:00:59 +0000

    Removed unused codesets.library-specific declarations from lib_protos.h.

--- source/Modules/icon/init/lib_protos.h
+++ source/Modules/icon/init/lib_protos.h
@@ -10,56 +10,7 @@
 ULONG freeBase(struct LibraryHeader* lib);
 ULONG initBase(struct LibraryHeader* lib);
 
-/* utils.c */
-#if defined(__amigaos4__)
-  #define HAVE_ALLOCVECPOOLED 1
-  #define HAVE_FREEVECPOOLED  1
-  #define HAVE_GETHEAD        1
-  #define HAVE_GETTAIL        1
-  #define HAVE_GETPRED        1
-  #define HAVE_GETSUCC        1
-#elif defined(__MORPHOS__)
-  #define HAVE_ALLOCVECPOOLED 1
-  #define HAVE_FREEVECPOOLED  1
-  #define HAVE_GETHEAD        1
-  #define HAVE_GETTAIL        1
-  #define HAVE_GETPRED        1
-  #define HAVE_GETSUCC        1
-#elif defined(__AROS__)
-  #define HAVE_ALLOCVECPOOLED 1
-  #define HAVE_FREEVECPOOLED  1
-  #define HAVE_GETHEAD        1
-  #define HAVE_GETTAIL        1
-  #define HAVE_GETPRED        1
-  #define HAVE_GETSUCC        1
-#endif
 
-#if defined(HAVE_ALLOCVECPOOLED)
-#define allocVecPooled(pool,size) AllocVecPooled(pool,size)
-#else
-APTR allocVecPooled(APTR pool, ULONG size);
-#endif
-#if defined(HAVE_FREEVECPOOLED)
-#define freeVecPooled(pool,mem)   FreeVecPooled(pool,mem)
-#else
-void freeVecPooled(APTR pool, APTR mem);
-#endif
-APTR reallocVecPooled(APTR pool, APTR mem, ULONG oldSize, ULONG newSize);
-APTR allocArbitrateVecPooled(ULONG size);
-void freeArbitrateVecPooled(APTR mem);
-APTR reallocArbitrateVecPooled(APTR mem, ULONG oldSize, ULONG newSize);
-#if !defined(HAVE_GETHEAD)
-struct Node *GetHead(struct List *list);
-#endif
-#if !defined(HAVE_GETPRED)
-struct Node *GetPred(struct Node *node);
-#endif
-#if !defined(HAVE_GETSUCC)
-struct Node *GetSucc(struct Node *node);
-#endif
-#if !defined(HAVE_GETTAIL)
-struct Node *GetTail(struct List *list);
-#endif
 
 // protos:
 
