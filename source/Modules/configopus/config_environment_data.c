321ced8df657323fb125753745e92a13ee56954b
itix <itix@fbfc52eb-d3ae-4490-b8fb-8982a1084939>
2015-09-20 07:29:17 +0000

    Seems declaring static function inside function is no longer allowed...

--- source/Modules/configopus/config_environment_data.c
+++ source/Modules/configopus/config_environment_data.c
@@ -10,10 +10,10 @@ ConfigWindow
 
 // Palette slider callback hook
 #if defined(__MORPHOS__)
+static void _palette_slider_callback_entry(struct TagItem *tag, struct Window *window);
+
 static void _palette_slider_callback_trampoline()
 {
-	static void _palette_slider_callback_entry(struct TagItem *tag, struct Window *window);
-
 	struct TagItem *tag = (struct TagItem *)REG_A1;
 	struct Window *window = (struct Window *)REG_A2;
 	_palette_slider_callback_entry(tag, window);
