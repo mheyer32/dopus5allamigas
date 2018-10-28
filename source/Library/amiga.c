10b8c0ae408673d8b20a7f94564ee2fef3f7d99a
mheyer <sonode@gmx.de>
2018-10-28 15:05:05 -0700

    Fix gcc warning about misleading indentation

--- source/Library/amiga.c
+++ source/Library/amiga.c
@@ -1,15 +1,15 @@
 /* Replacement functions */
 
-
 #if !defined(__MORPHOS__) && !defined(__AROS__)
 int stccpy(char *p, const char *q, int n)
 {
 	char *t = p;
 
-	while ((*p++ = *q++) && --n > 0);
-		p[-1] = '\0';
+	while ((*p++ = *q++) && --n > 0)
+	{
+	};
+	p[-1] = '\0';
 
 	return p - t;
 }
 #endif
-
