1aef8504c410ae70365acbe390e365536c8cfd7b
mheyer <sonode@gmx.de>
2018-11-12 22:32:47 -0800

    Fix warning about misleading indentation

--- source/Modules/ftp/init/amiga.c
+++ source/Modules/ftp/init/amiga.c
@@ -7,8 +7,9 @@ int stccpy(char *p, const char *q, int n)
 {
 	char *t = p;
 
-	while ((*p++ = *q++) && --n > 0);
-		p[-1] = '\0';
+	while ((*p++ = *q++) && --n > 0)
+	{};
+	p[-1] = '\0';
 
 	return p - t;
 }
