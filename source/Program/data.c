8858f7fe2e928215a77fe9296a147a1ff9b1384d
mheyer <sonode@gmx.de>
2023-01-03 14:33:27 -0800

    Update to NDK3.2
    UtilityBase is now declared as 'struct Library*', as it should be.
    cybergraphics.h also seems to have moved.

--- source/Program/data.c
+++ source/Program/data.c
@@ -73,7 +73,7 @@ struct AmigaGuideIFace *IAmigaGuide = NULL;
 #if defined(__amigaos4__) || defined(__AROS__)
 struct UtilityBase *UtilityBase = NULL;
 #else
-struct UtilityBase *UtilityBase = NULL;
+struct Library *UtilityBase = NULL;
 #endif
 struct IntuitionBase *IntuitionBase = NULL;
 struct GfxBase *GfxBase = NULL;
